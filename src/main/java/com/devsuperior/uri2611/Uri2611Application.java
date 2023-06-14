901db6b23aa771e489f8b043e038cd61ac2b3c1f
Adriano Vicente <adrianovic.silva@gmail.com>
2023-06-14 19:30:53 -0300

    first commit

---
+++ src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -0,0 +1,40 @@
+package com.devsuperior.uri2611;
+
+import com.devsuperior.uri2611.dto.MovieMinDTO;
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+import com.devsuperior.uri2611.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Uri2611Application implements CommandLineRunner {
+
+	@Autowired
+	private MovieRepository movieRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<MovieMinProjection> list = movieRepository.search1("Action");
+		List<MovieMinDTO> result1 = list.stream().map(x ->
+				new MovieMinDTO(x)).collect(Collectors.toList());
+		System.out.println("SQL");
+		for (MovieMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+
+		List<MovieMinDTO> result2 = movieRepository.search2("Action");
+		System.out.println("JPQL");
+		for (MovieMinDTO obj : result2) {
+			System.out.println(obj);
+		}
+	}
+}
